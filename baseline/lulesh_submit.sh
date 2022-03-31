56215c5d25b996b9f2490a475b04e37c1913f642
Marcin Copik <mcopik@gmail.com>
2022-03-31 19:50:22 +0200

    Add LULESH scripts

---
+++ baseline/lulesh_submit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for ranks_per_node in 32 28 24 20 16 8 1; do
+	sbatch -n 64 < slurm_lulesh_${ranks_per_node}.sh
+done
--- baseline/slurm_lulesh.sh
+++ baseline/slurm_lulesh.sh
@@ -9,6 +9,8 @@
 #SBATCH --partition=normal
 #SBATCH --constraint=mc
 #SBATCH --hint=nomultithread
+#SBATCH --output=lulesh.%a.o
+#SBATCH --error=lulesh.%a.e
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 #export CRAY_CUDA_MPS=1
@@ -29,16 +31,14 @@ export OUTPUT_DIR=${RESULT_DIR}
 cd ${OUTPUT_DIR}
 mkdir -p lulesh_${RANKS_PER_NODE}
 cd lulesh_${RANKS_PER_NODE}
-for size in 15 20 25 30 35; do
-#for size in 20; do
+for size in 10 15 18 20 25; do
 
-	#for rep in 0 1 2 3 4 5 6 7 8 9; do
-	for rep in 0 1; do
+	for rep in 0 1 2 3 4 5 6 7 8 9; do
 		START=$(date +%s.%N)
-		srun ${BINARY_DIR}/lulesh2.0 -s ${size} > lulesh_${size}_${rep}.out 2>&1
+		srun /usr/bin/time --append --output-file=lulesh_${size}.times ${BINARY_DIR}/lulesh2.0 -s ${size} > lulesh_${size}_${rep}.out 2>&1
 		END=$(date +%s.%N)
 		DIFF=$(echo "$END - $START" | bc)
-		echo $DIFF >> lulesh_${size}_${rep}.times
+		echo $DIFF >> lulesh_${size}.full.times
 	done
 
 done
