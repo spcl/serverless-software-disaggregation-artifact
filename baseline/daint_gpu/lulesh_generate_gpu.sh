bcf7daebafbdf61dcf7cc1cc17504f9515c0ad67
Marcin Copik <mcopik@gmail.com>
2022-05-13 20:56:48 +0200

    Modify LULESH scripts for GPU nodes

--- baseline/daint_gpu/lulesh_generate_gpu.sh
+++ baseline/daint_gpu/lulesh_generate_gpu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-for ranks_per_node in 12 10 8 6 4 2; do
-	LULESH_DIR=/scratch/snx3000/mcopik/serverless_hpc/serverless-software-disaggregation-artifact/external/build_lulesh/ RESULT_DIR=$(pwd)/../data/baseline RANKS_PER_NODE=${ranks_per_node} envsubst '${LULESH_DIR},${RESULT_DIR},${RANKS_PER_NODE}' < slurm_lulesh_gpu.sh > slurm_lulesh_gpu_${ranks_per_node}.sh
+ARTIFACT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../..
+
+for ranks_per_node in 12 10 9 8 6 4 2; do
+	LULESH_DIR=${ARTIFACT_DIR}/external/LULESH/build_gpu RESULT_DIR=$(pwd)/../data/baseline/daint_gpu RANKS_PER_NODE=${ranks_per_node} envsubst '${LULESH_DIR},${RESULT_DIR},${RANKS_PER_NODE}' < slurm_lulesh_gpu.sh > slurm_lulesh_gpu_${ranks_per_node}.sh
 done
--- baseline/daint_gpu/slurm_lulesh_gpu.sh
+++ baseline/daint_gpu/slurm_lulesh_gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --job-name="lulesh"
 #SBATCH --account="g34"
-#SBATCH --time=04:00:00
+#SBATCH --time=00:30:00
 ##SBATCH --nodes=1
 #SBATCH --ntasks-per-core=1
 #SBATCH --ntasks-per-node=${RANKS_PER_NODE}
@@ -9,8 +9,9 @@
 #SBATCH --partition=normal
 #SBATCH --constraint=gpu
 #SBATCH --hint=nomultithread
-#SBATCH --output=lulesh.%a.o
-#SBATCH --error=lulesh.%a.e
+#SBATCH --output=lulesh.%A.%a.o
+#SBATCH --error=lulesh.%A.%a.e
+#SBATCH --array=[0-19]
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 #export CRAY_CUDA_MPS=1
@@ -29,17 +30,18 @@ export BINARY_DIR=${LULESH_DIR}
 export OUTPUT_DIR=${RESULT_DIR}
 
 cd ${OUTPUT_DIR}
-mkdir -p lulesh_gpu_${RANKS_PER_NODE}
-cd lulesh_gpu_${RANKS_PER_NODE}
-for size in 10 15 18 20 25; do
+mkdir -p lulesh_${SLURM_NTASKS}
+cd lulesh_${SLURM_NTASKS}
+mkdir -p lulesh_${RANKS_PER_NODE}
+cd lulesh_${RANKS_PER_NODE}
+for size in 15 18 20 25; do
 
-	for rep in 0 1 2 3 4 5 6 7 8 9; do
-		START=$(date +%s.%N)
-		srun /usr/bin/time --append --output-file=lulesh_${size}.times ${BINARY_DIR}/lulesh2.0 -s ${size} > lulesh_${size}_${rep}.out 2>&1
-		END=$(date +%s.%N)
-		DIFF=$(echo "$END - $START" | bc)
-		echo $DIFF >> lulesh_${size}.full.times
-	done
+	rep=${SLURM_ARRAY_TASK_ID}
+	START=$(date +%s.%N)
+	srun /usr/bin/time --append --output-file=lulesh_${size}.times ${BINARY_DIR}/lulesh2.0 -s ${size} > lulesh_${size}_${rep}.out 2>&1
+	END=$(date +%s.%N)
+	DIFF=$(echo "$END - $START" | bc)
+	echo $DIFF >> lulesh_${size}.full.times
 
 done
 
