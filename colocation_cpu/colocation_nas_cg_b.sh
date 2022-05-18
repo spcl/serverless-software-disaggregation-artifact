5e492aab647b7f6b1a807b93fd97db07ba6109f4
Marcin Copik <mcopik@gmail.com>
2022-05-18 02:50:58 +0200

    Update scripts

--- colocation_cpu/colocation_nas_cg_b.sh
+++ colocation_cpu/colocation_nas_cg_b.sh
@@ -3,4 +3,4 @@
 NODE_FAAS=$1
 EXPERIMENT=$2
 
-./${EXPERIMENT}_experiment.sh "${NODE_FAAS}" "cg" "B" 4 2 8
+./${EXPERIMENT}_experiment.sh "${NODE_FAAS}" "cg" "B" 4 2 8 "0 1 2 3 4 5 6 7 8 9"
--- colocation_cpu/milc_experiment.sh
+++ colocation_cpu/milc_experiment.sh
@@ -7,6 +7,7 @@ experiment_size=$3
 experiment_cores=$4
 experiment_nodes=$5
 experiment_tasks=$6
+REPS=$7
 
 ARTIFACT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../
 DATA_DIR=${ARTIFACT_DIR}/data/colocation_cpu_precision/milc
@@ -28,14 +29,15 @@ echo "Running benchmark ${experiment_name} with size ${experiment_size}, on ${ex
 
 module load sarus
 
-for rep in 0 1 2 3 4 5 6 7 8 9; do
+#for rep in 0 1 2 3 4 5 6 7 8 9; do
+for rep in $REPS; do
   for size in 32 64 96 128; do
 
 		echo "Start $size, repetition $rep"
 		mkdir -p nas_${size}_${experiment_name}_${experiment_size}
 		pushd nas_${size}_${experiment_name}_${experiment_size}
 
-		OMP_NUM_THREADS=1 nohup srun --cpu-bind=verbose  --ntasks-per-core=1 --hint=nomultithread  --exclusive=user --oversubscribe --cpu-bind=verbose -e milc_${size}_${rep}.err -o milc_${size}_${rep}.out -N 2 -n ${milc_ranks} /usr/bin/time --append --output-file=../milc_${size}.times ${MILC_DIR}/su3_rmd ${MILC_INPUT_DIR}/milc.p${milc_ranks}.size${size}.in > ../milc_${size}_${rep}.out &
+		OMP_NUM_THREADS=1 nohup srun --cpu-bind=verbose  --ntasks-per-core=1 --hint=nomultithread  --exclusive=user --oversubscribe --cpu-bind=verbose -e milc_${size}_${rep}.err -o milc_${size}_${rep}.out -N 2 -n ${milc_ranks} /usr/bin/time --append --output-file=../milc_${size}.times ${MILC_DIR}/su3_rmd ${MILC_INPUT_DIR}/milc.p${milc_ranks}.size${size}.in &
     echo "Submitted MILC, starting SARUS jobs"
     
 		PID=$!
