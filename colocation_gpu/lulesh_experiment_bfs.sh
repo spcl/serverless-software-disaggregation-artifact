7db7c925aeaace6008fe300066db2ac041efe6b8
Marcin Copik <mcopik@gmail.com>
2022-05-18 23:46:00 +0200

    Update GPU co-location scripts

--- colocation_gpu/lulesh_experiment_bfs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-lulesh_p=64
-NODE_FAAS=$1
-
-./lulesh_experiment7.sh "${NODE_FAAS}" "bfs" 1 1 1
--- colocation_gpu/lulesh_experiment_hotspot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-lulesh_p=64
-NODE_FAAS=$1
-
-./lulesh_experiment4.sh "${NODE_FAAS}" "hotspot" 1 1 1
