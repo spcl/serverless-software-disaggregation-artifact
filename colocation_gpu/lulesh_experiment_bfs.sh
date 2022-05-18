8e282e97dc9f31e21609af78598a30093c8ba9bc
Marcin Copik <mcopik@gmail.com>
2022-05-18 02:25:44 +0200

    Add GPU colocation scripts

---
+++ colocation_gpu/lulesh_experiment_bfs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+lulesh_p=64
+NODE_FAAS=$1
+
+./lulesh_experiment7.sh "${NODE_FAAS}" "bfs" 1 1 1
---
+++ colocation_gpu/lulesh_experiment_srad.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+lulesh_p=64
+NODE_FAAS=$1
+
+./lulesh_experiment6.sh "${NODE_FAAS}" "srad-v1" 1 1 1
