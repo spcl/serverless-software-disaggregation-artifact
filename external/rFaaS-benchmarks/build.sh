3738886320552fc86b382b327371b18e6a51abdc
Marcin Copik <mcopik@gmail.com>
2023-10-24 18:39:00 +0200

    wip

--- external/rFaaS-benchmarks/build.sh
+++ external/rFaaS-benchmarks/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-docker build -f images/Dockerfile.opencv -t spcleth/hpc-disagg:rfaas-dependencies-opencv
-docker build -f images/Dockerfile.torchvision -t spcleth/hpc-disagg:rfaas-dependencies-torchvision-cpu
+docker build -f images/Dockerfile.opencv -t spcleth/hpc-disagg:rfaas-dependencies-opencv .
+docker build -f images/Dockerfile.torchvision -t spcleth/hpc-disagg:rfaas-dependencies-torchvision-cpu .
 docker build -f images/Dockerfile.image-recognition -t spcleth/hpc-disagg:rfaas-functions-image-recognition .
 docker build -f images/Dockerfile.thumbnailer -t spcleth/hpc-disagg:rfaas-functions-thumbnailer .
-
